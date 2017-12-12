a44ded11d2fd7a298913041aad891d2840fbd896
Chris Luke <chris_luke@cable.comcast.com>
2017-12-11 20:16:50 -0800

    Day 5, part 2

---
+++ day5/part2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+
+try:
+	input_file = sys.argv[1]
+except:
+	input_file = "input.txt"
+
+with open(input_file) as fp:
+	program = [int(v.strip()) for v in fp]
+
+ip = 0
+steps = 0
+while ip < len(program):
+	old_ip = ip
+	ip += program[ip]
+	if program[old_ip] < 3:
+		program[old_ip] += 1
+	else:
+		program[old_ip] -= 1
+	steps += 1
+
+print "steps: %d" % steps
