1da0115d608aea10531dcaa9eec1c32d87c8f1a2
Chris Luke <chris_luke@cable.comcast.com>
2017-12-12 16:58:21 -0800

    Day 9, part 2

---
+++ day9/part2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import sys
+
+try:
+	input_file = sys.argv[1]
+except:
+	input_file = "input.txt"
+
+streams = []
+with open(input_file) as fp:
+	for line in fp:
+		streams.append(line.strip())
+
+for stream in streams:
+	groups = 0
+	score = 0
+	tally = 0
+
+	skip = False
+	garbage = False
+	trash = 0
+
+	for ch in list(stream):
+		if skip:
+			skip = False
+			continue
+
+		if ch == "!":
+			skip = True
+			continue
+
+		if garbage:
+			if ch == ">":
+				garbage = False
+			else:
+				trash += 1
+		else:
+			if ch == "{":
+				groups += 1
+				tally += 1
+			elif ch == "}":
+				score += tally
+				tally -= 1
+			elif ch == "<":
+				garbage = True
+
+
+	print "groups: %d score: %d trash: %d" % (groups, score, trash)
